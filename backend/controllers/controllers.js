3e6a38e1acd241dc5c06af47905ed67ecfa5668d
Jom Karlo Verzosa <jomkarlov@gmail.com>
2023-06-16 13:04:30 +0800

    Added search functionality

--- backend/controllers/controllers.js
+++ backend/controllers/controllers.js
@@ -2,15 +2,27 @@ const Item = require("../models/itemModel")
 
 const getHome = async (req, res) => {
     let isLoggedIn = req.session.isLoggedIn || false;
+    const query = ""
     try {
         const items = await Item.find().exec();
-        res.render('home.ejs', {isLoggedIn, items});
+        res.render('home.ejs', {isLoggedIn, items, query});
     } catch (error) {
         console.log("Error retrieving items", error);
         res.status(500).send
     }
 }
 
-module.exports = {
-    getHome
+const getSearch = async (req, res) => {
+    let isLoggedIn = req.session.isLoggedIn || false;
+    const query = req.query.query
+    try {
+        const items = await Item.find( {name: { $regex: query,  $options: 'i'}}).exec()
+
+        res.render('home.ejs', {isLoggedIn, items, query})
+    } catch(error) {
+        console.log("Error searching for item")
+        res.status(500).send
+    }
 }
+
+module.exports = { getHome, getSearch }
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -4,26 +4,24 @@ const router = express.Router()
 const multer = require("multer")
 const upload = multer()
 
-const { getHome } = require('../controllers/controllers')
+const { getHome, getSearch } = require('../controllers/controllers')
 const { getItem, getCart, postHome, postCart, deleteItem, removeCartItem } = require("../controllers/itemControllers")
 const { getLogin, getSignup, getLogout, postSignup, postLogin } = require("../controllers/userControllers")
 
 router.route('/').get(getHome).post(upload.single('image') ,postHome)
-
-router.route('/cart').get(getCart)
-
-router.route('/cart/:id/removeItem').post(removeCartItem)
-
-router.route('/item/:id').get(getItem)
-
-router.route('/item/:id/removeItem').post(deleteItem)
-
-router.route('/item/:id/addToCart').post(postCart)
-
-router.route('/logout').get(getLogout)
+router.route('/search').get(getSearch)
 
 router.route('/login').get(getLogin).post(postLogin)
-
 router.route('/signup').get(getSignup).post(postSignup)
+router.route('/logout').get(getLogout)
+
+// Items
+router.route('/item/:id').get(getItem)
+router.route('/item/:id/removeItem').post(deleteItem)
+router.route('/item/:id/addToCart').post(postCart)
+
+// Cart
+router.route('/cart').get(getCart)
+router.route('/cart/:id/removeItem').post(removeCartItem)
 
 module.exports = router
