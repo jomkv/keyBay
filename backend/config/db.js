99915c632c7c2284c0a39c9ecdb498d814ec7bdf
Jom Karlo Verzosa <jomkarlov@gmail.com>
2023-07-19 12:59:11 +0800

    Add: db connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,2 +1,13 @@
 const mongoose = require("mongoose");
+const colors = require("colors");
 
+exports.connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`DB Connected: ${conn.connection.host}`.cyan.underline);
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 3000;
+const { errorHandler } = require("./middleware/errorMiddleware");
+const { connectDB } = require("./config/db");
+
+connectDB();
 
 const app = express();
+
+// ROUTES TODO
+
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log("Server now running!");
